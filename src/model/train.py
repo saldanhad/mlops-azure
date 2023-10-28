50be7d092b182b695cad1eae2117a858772cdd34
Deepak Saldanha <115748404+saldanhad@users.noreply.github.com>
2023-10-28 19:43:27 -0400

    add code for the TO DO scripts

--- src/model/train.py
+++ src/model/train.py
@@ -3,16 +3,14 @@
 import argparse
 import glob
 import os
-
 import pandas as pd
-
 from sklearn.linear_model import LogisticRegression
-
+from sklearn.model_selection import train_test_split
 
 # define functions
 def main(args):
     # TO DO: enable autologging
-
+    mlflow.azureml.autolog()
 
     # read data
     df = get_csvs_df(args.training_data)
@@ -34,7 +32,11 @@ def get_csvs_df(path):
 
 
 # TO DO: add function to split data
-
+def split_data(df):
+    X = df[['Pregnancies','PlasmaGlucose','DiastolicBloodPressure','TricepsThickness','SerumInsulin','BMI','DiabetesPedigree','Age']].values
+    y = df['Diabetic'].values
+    X_train, X_test, y_trai, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+    return X_train, X_test, y_train, y_test 
 
 def train_model(reg_rate, X_train, X_test, y_train, y_test):
     # train model
