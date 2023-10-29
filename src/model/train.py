3d44bd54e6ce414c19d5c843f4003378aab5a441
Deepak Saldanha <115748404+saldanhad@users.noreply.github.com>
2023-10-29 18:20:27 -0400

    Update train.py

--- src/model/train.py
+++ src/model/train.py
@@ -8,23 +8,20 @@ from sklearn.linear_model import LogisticRegression
 from sklearn.model_selection import train_test_split
 import mlflow
 
-# Define functions
 
+# Define functions
 def main(args):
     # TO DO: enable autologging
     mlflow.azureml.autolog()
-
     # Read data
     df = get_csvs_df(args.training_data)
-
     # Split data
     X_train, X_test, y_train, y_test = split_data(df)
-
     # Train model
     train_model(args.reg_rate, X_train, X_test, y_train, y_test)
 
 
-def get_csvs_df(path): 
+def get_csvs_df(path):
     if not os.path.exists(path):
         raise RuntimeError(f"Cannot use a non-existent path provided: {path}")
     csv_files = glob.glob(f"{path}/*.csv")
@@ -33,11 +30,11 @@ def get_csvs_df(path):
     return pd.concat((pd.read_csv(f) for f in csv_files), sort=True)  # Set sort=True if column order matters
 
 
+
 # TO DO: add a function to split data
 def split_data(df):
     X = df[['Pregnancies', 'PlasmaGlucose', 'DiastolicBloodPressure', 'TricepsThickness', 'SerumInsulin', 'BMI', 'DiabetesPedigree', 'Age']].values
     y = df['Diabetic'].values
-    
     X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=123)
     return X_train, X_test, y_train, y_test
 
@@ -50,29 +47,24 @@ def train_model(reg_rate, X_train, X_test, y_train, y_test):
 def parse_args():
     # Setup arg parser
     parser = argparse.ArgumentParser()
-
     # Add arguments
     parser.add_argument("--training_data", dest='training_data', type=str)
     parser.add_argument("--reg_rate", dest='reg_rate', type=float, default=0.01)
-
     # Parse args
     args = parser.parse_args()
-
     # Return args
     return args
+
 
 # Run script
 if __name__ == "__main__":
     # Add space in logs
     print("\n\n")
     print("*" * 60)
-
     # Parse args
     args = parse_args()
-
     # Run the main function
     main(args)
-
     # Add space in logs
     print("*" * 60)
     print("\n\n")
