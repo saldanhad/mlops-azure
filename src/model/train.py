846645ee13a3ea0ce656c74df3e93dd01ef66c13
Deepak Saldanha <115748404+saldanhad@users.noreply.github.com>
2023-10-29 18:24:13 -0400

    Update train.py

--- src/model/train.py
+++ src/model/train.py
@@ -27,20 +27,20 @@ def get_csvs_df(path):
     csv_files = glob.glob(f"{path}/*.csv")
     if not csv_files:
         raise RuntimeError(f"No CSV files found in path: {path}")
-    return pd.concat((pd.read_csv(f) for f in csv_files), sort=True)  # Set sort=True if column order matters
+    return pd.concat((pd.read_csv(f) for f in csv_files), sort=True) 
 
 
 
-# TO DO: add a function to split data
+# TO DO:function to split data
 def split_data(df):
-    X = df[['Pregnancies', 'PlasmaGlucose', 'DiastolicBloodPressure', 'TricepsThickness', 'SerumInsulin', 'BMI', 'DiabetesPedigree', 'Age']].values
+    X = df[['Pregnancies', 'PlasmaGlucose', 'DiastolicBloodPressure', 'TricepsThickness',
+    'SerumInsulin', 'BMI', 'DiabetesPedigree', 'Age']].values
     y = df['Diabetic'].values
     X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=123)
     return X_train, X_test, y_train, y_test
 
 
 def train_model(reg_rate, X_train, X_test, y_train, y_test):
-    # Train model
     LogisticRegression(C=1/reg_rate, solver="liblinear").fit(X_train, y_train)
     
 
@@ -49,7 +49,8 @@ def parse_args():
     parser = argparse.ArgumentParser()
     # Add arguments
     parser.add_argument("--training_data", dest='training_data', type=str)
-    parser.add_argument("--reg_rate", dest='reg_rate', type=float, default=0.01)
+    parser.add_argument("--reg_rate", dest='reg_rate', 
+                        type=float, default=0.01)
     # Parse args
     args = parser.parse_args()
     # Return args
