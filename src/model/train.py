85b1446a140d5a003e30ee955ca7f2f1fb4d5688
Deepak Saldanha <115748404+saldanhad@users.noreply.github.com>
2023-10-29 18:04:30 -0400

    Update train.py (#5)

--- src/model/train.py
+++ src/model/train.py
@@ -21,7 +21,7 @@ def main(args):
     X_train, X_test, y_train, y_test = split_data(df)
 
     # Train model
-    model = train_model(args.reg_rate, X_train, X_test, y_train, y_test)
+    train_model(args.reg_rate, X_train, X_test, y_train, y_test)
 
 def get_csvs_df(path):
     
